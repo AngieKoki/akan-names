757fd9ad6f12c1c411a6d18dbe96affe756af2e5
Angie Koki <angie.dar66@gmail.com>
2020-07-19 23:40:41 +0300

    Modify control flow with for loop and if else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,27 @@
-// ARRAY FOR NAMES
-girlAkan=['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
-boyAkan=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw','Kofi', 'Kwame'];
-//DAY OF THE WEEK FUNCTION
-const weekDay = document.getElementById('dob').oninput.getDay();
-    //let cc = ('yyyy.parseInt(charAt(0,1))');
-    //const weekDay=('(((cc/4))-2*cc-1)+((5*yy/4))+((26*(mm+1)(10))+dd)%7');
-    
-const gender = document.getElementById('gender').onselect;
+function getweekDay(){
 
+var days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var girlAkan=['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var boyAkan=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw','Kofi', 'Kwame'];
+var myGender=document.getElementsByName('gender');
 
-//CONTROL FLOW
-if (weekDay === 0 && gender ==='female'){
-    console.log(girlAkan[0]);
+var weekDay=document.getElementById('dob').value;
+var dateOfBirth = new Date(weekDay);
+var dayOfTheWeek = dateOfBirth.getDay()
+if(dayOfTheWeek === ''){
+    console.log('invalid birthday!')
 }
-else if (weekDay === 1 && gender ==='female') {
-    console.log(girlAkan[1]);
+else {
+    for(var i=0;i<myGender.length;i++){
+        if(myGender[i].checked){
+            if(myGender[i].value === 'Male'){
+                console.log('Born on a' + days[dayOfTheWeek] + 'your Akan Name is ' + boyAkan[dayOfTheWeek]);
+            }
+            else{
+            console.log('Born on a ' + days[dayOfTheWeek] + 'your Akan Name is ' + girlAkan[dayOfTheWeek]);
+            break;
+            }
+        }
+    }
 }
-else if (weekDay === 2 && gender ==='female') {
-    console.log(girlAkan[2]);
-} 
-else if (weekDay === 3 && gender ==='female') {
-    console.log(girlAkan[3]);
-} 
-else if (weekDay === 4 && gender ==='female') {
-    console.log(girlAkan[4]);
-} 
-else if (_weekDay === 5 && gender ==='female') {
-    console.log(girlAkan[5]);
-} 
-else if (weekDay === 6 && gender ==='female') {
-    console.log(girlAkan[6]);
-} 
-else if (weekDay === 0 && gender ==='male') {
-    console.log(boyAkan[0]);
-} 
-else if (weekDay === 1 && gender ==='male') {
-   console.log(boyAkan[1]);
-} 
-else if (weekDay === 2 && gender ==='male') {
-    console.log(boyAkan[2]);
-}  
-else if (weekDay === 3 && gender ==='male') {
-    console.log(boyAkan[3]);
-}  
-else if (weekDay === 4 && gender ==='male') {
-    console.log(boyAkan[4]);
-}  
-else if (weekDay === 5 && gender ==='male') {
-    console.log(boyAkan[5]);
-}  
-else if (weekDay === 6 && gender ==='male') {
-    console.log(boyAkan[6]);
-}  
-else { 
-    console.log('Input neccessary field');
-    
-}
-
-//manipulation
-
-//button
-document.getElementsByClassName('btn').onclick = function(){
-    document.getElementById('form').innerHTML = 'Your Akan name is';
-    document.getElementById('btn').style.display = 'none';
 }
