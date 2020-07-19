4de2bfb0df53e2d36f254ae3ba2f9ccec052e603
Angie Koki <angie.dar66@gmail.com>
2020-07-19 16:10:30 +0300

    Add lICENSE.txt file Amend README.md file - add BDD content Modify submit button is scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@
 girlAkan=['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
 boyAkan=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw','Kofi', 'Kwame'];
 //DAY OF THE WEEK FUNCTION
-const dayOfWeek=(((CC/4))-2*CC-1)+((5*YY/4))+((26*(MM+1)(10))+DD)%7
+//const dayOfWeek=(((CC/4))-2*CC-1)+((5*YY/4))+((26*(MM+1)(10))+DD)%7;
+
 //CONTROL FLOW
-if dayOfWeek =0 {
-    
+
+//manipulation
+
+//button
+document.getElementById('btn').onclick = function(){
+    document.getElementById('form').innerHTML = 'Your Akan name is';
+    document.getElementById('btn').style.display = 'none';
 }
