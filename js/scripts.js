6d4d5efbbf1d166e8fc23e53cf28eed7270fd97c
Angie Koki <angie.dar66@gmail.com>
2020-07-19 13:42:17 +0300

    Ammend gender selection form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 // ARRAY FOR NAMES
 girlAkan=['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
-boyAkan=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw','Kofi',]
+boyAkan=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw','Kofi', 'Kwame'];
+//DAY OF THE WEEK FUNCTION
+const dayOfWeek=(((CC/4))-2*CC-1)+((5*YY/4))+((26*(MM+1)(10))+DD)%7
+//CONTROL FLOW
+if dayOfWeek =0 {
+    
+}
